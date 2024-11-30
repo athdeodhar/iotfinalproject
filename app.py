21ca07af0f092fa914d21b85736ffcc2c14d4ae7
Atharva Deodhar <atharvadeodhar@Atharvas-MacBook-Pro.local>
2024-11-30 14:25:22 -0800

    Nilabjya code and remove icons after inactivity

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from time import time
+
+game_state=False
 
 app = Flask(__name__)
 CORS(app)  # Enable cross-origin requests (optional)
 user_locations = {}  # In-memory storage for user locations
+last_seen = {}
 
 # Serve the main HTML page
 @app.route('/')
@@ -22,6 +26,20 @@ def update_location():
         return jsonify({"error": "Invalid data"}), 400
 
     user_locations[user_id] = {"lat": lat, "lng": lng}
+    last_seen[user_id] = int(time())
+    to_delete = []
+    for u in user_locations:
+        if u == "hider":
+            continue
+        tim = int(time())
+        print(tim)
+        print(last_seen[u])
+        if tim - last_seen[u] > 60:
+            to_delete.append(u)
+    for u in to_delete:
+        del last_seen[u]
+        del user_locations[u]
+            
     return jsonify({"message": "Location updated"}), 200
 
 # API to update hider's location
@@ -35,6 +53,17 @@ def hider():
 def get_locations():
     user_locations["hider"] = {"speed": 10, "lat": 33.6472, "lng": -117.8411}
     return jsonify(user_locations), 200
+
+@app.route('/set_game_state', methods=['POST'])
+def set_game_state():
+    global game_state
+    if request.form.get("state") == "True":
+        print("Setting game_state to True")
+        game_state=True
+    elif request.form.get("state") == "False":
+        print("Setting game_state to False")
+        game_state=False
+    return str(game_state)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
